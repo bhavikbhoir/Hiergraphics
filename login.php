ae516f833e6705ae956d01b8c349c8180e44b606
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2019-11-26 21:26:16 -0500

    Update login.php

--- login.php
+++ login.php
@@ -23,7 +23,7 @@
 		$rows = mysqli_num_rows($result);
         if($rows==1){
 			$_SESSION['username'] = $username;
-			header("Location: index.html"); // Redirect user to index.php
+			header("Location: index.php"); // Redirect user to index.php
             }else{
 				echo "<div class='form'><h3>Username/password is incorrect.</h3><br/>Click here to <a href='login.php'>Login</a></div>";
 				}
