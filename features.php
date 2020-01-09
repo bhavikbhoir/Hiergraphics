14d3f82051c0fad94724d847e45ff598838acf10
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2020-01-09 14:25:36 -0500

    Update features.php

--- features.php
+++ features.php
@@ -1,91 +1,72 @@
 <?php
-include("auth.php"); //include auth.php file on all secure pages 
+include("auth.php");
 ?>
 <!DOCTYPE html> 
 <html lang="en"> 
 <head> 
-	<title>Hiergraphics</title> 
+<title>Hiergraphics</title> 
 	
-	<meta charset="utf-8"> 
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<meta charset="utf-8"> 
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
 
-	<link rel="stylesheet" href= 
-"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
 	
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-	<script src= 
-"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> 
-	</script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
 	
-	<script src= 
-"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"> 
-	</script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script> 
 	
-	<script src= 
-"https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"> 
-	</script> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> 
 
-	<link rel="stylesheet" href="features.css">
+<link rel="stylesheet" href="features.css">
 </head> 
 
-<body> 
+<body>
 	
-	<div class="header">  
-		<div class="logo">
-			<img src="logo.png" style="align-content: center;width: 200px;">
-		</div>
+<div class="header">  
+	<div class="logo">
+		<img src="logo.png" style="align-content: center;width: 200px;">
+	</div>
 	
-		<nav class="navbar navbar-expand-sm navbar-dark"> 
+	<nav class="navbar navbar-expand-sm navbar-dark"> 
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapse_Navbar" > 
+			<span class="navbar-toggler-icon"></span> 
+		</button> 
 			
-			<!-- Brand/logo -->
-			<!--
-			<a class="navbar-brand" href="#"> 
-				<img src= "https://www.geeksforgeeks.org/wp-content/uploads/gfg_transparent_white_small.png"
-				alt="logo" style="width:40px;"> 
-			</a> 
-		-->
-			
-			<button class="navbar-toggler" type="button" data-toggle="collapse"
-						data-target="#collapse_Navbar" > 
-				<span class="navbar-toggler-icon"></span> 
-			</button> 
-			
-			<div class="collapse navbar-collapse justify-content-center" id="collapse_Navbar"> 
-				<ul class="navbar-nav"> 
-					<li class="nav-item"> 
-						<a class="nav-link" href="index.php">Home</a> 
-					</li> 
+		<div class="collapse navbar-collapse justify-content-center" id="collapse_Navbar"> 
+			<ul class="navbar-nav"> 
+				<li class="nav-item"> 
+					<a class="nav-link" href="index.php">Home</a> 
+				</li> 
 				
-					<li class="nav-item"> 
-						<a class="nav-link" href="features.php">Features</a> 
-					</li> 
+				<li class="nav-item"> 
+					<a class="nav-link" href="features.php">Features</a> 
+				</li> 
 				
-					<li class="nav-item"> 
-						<a class="nav-link" href="about.php">About</a> 
-					</li> 
+				<li class="nav-item"> 
+					<a class="nav-link" href="about.php">About</a> 
+				</li>
 
           			<li class="nav-item"> 
-           			 <a class="nav-link" href="research.php">Research</a> 
+           				<a class="nav-link" href="research.php">Research</a> 
           			</li> 
 				
-					<li class="nav-item dropdown" id="dropdown">
-	                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Welcome <?php echo $_SESSION['username']; ?>!</a>
+				<li class="nav-item dropdown" id="dropdown">
+	                    		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Welcome <?php echo $_SESSION['username']; ?>!</a>
 		            		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
 		              			<a class="dropdown-item" href="login.php"> Switch Account </a>
 		              			<a class="dropdown-item" href="logout.php"> Log Out </a>
 		            		</div>
-                	</li>
-				</ul> 
-			</div>
-		</nav> 
-	</div> 
-
+                		</li>
+			</ul> 
+		</div>
+	</nav> 
+</div> 
 
 <section id="Info">
-	
-	<div class="container">
+<div class="container">
   <div class="row">
     <div class="column-66">
       <h1 class="xlarge-font"><b>The App</b></h1>
@@ -128,8 +109,6 @@ include("auth.php"); //include auth.php file on all secure pages
     </div>
   </div>
 </div>
-
-
 </section>
 
 <!-- Footer -->
@@ -162,6 +141,5 @@ include("auth.php"); //include auth.php file on all secure pages
     </footer>
 
 </body> 
-
 </html>					 
 	
