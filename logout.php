5f1639ed03c1793d3c7eed22f16e7c8c2023d8ec
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2020-01-09 14:17:56 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ session_start();
 if(session_destroy()) // Destroying All Sessions
 {
 ob_start();	
-header("Location:index.html"); // Redirecting To Home Page
+header("Location:index.php"); // Redirecting To Home Page
 echo ob_get_clean();
 }
 ?>
